3764828da7aca3050b60cf0e57be1b3eaa3aa4a2
Kurt Hetrick <khetric1@jhmi.edu>
2018-03-09 14:30:24 -0500

    bug fix in push_sge_request, shouldn't filter out skel if skel is the source dummy

--- push_sge_request.sh
+++ push_sge_request.sh
@@ -1,3 +1,3 @@
 find /mnt/users -maxdepth 1 -mindepth 1 -type d \
 | awk '{print "cp -rvf", "/mnt/users/skel/.sge_request" , $1 "/priv" }' \
-| egrep -v "skight1|ktindal2|msherid3|jdicke15|skel"
+| egrep -v "/mnt/users/skel/priv|skight1|ktindal2|msherid3|jdicke15"
