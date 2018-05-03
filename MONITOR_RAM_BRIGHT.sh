ddefefda6e04a2ab8b3fd7b05a4b931bc7b6db10
Kurt Hetrick <khetric1@jhmi.edu>
2018-05-03 12:33:56 -0400

    MONITOR_RAM_BRIGHT.sh now works

--- MONITOR_RAM_BRIGHT.sh
+++ MONITOR_RAM_BRIGHT.sh
@@ -9,27 +9,30 @@ if [[ ! $REFRESH ]]
 	REFRESH=3600
 fi
 
-# need while statement to put this in a loop
-# need if/else statement to say whether to email or not
+# check the total memory using qhosts
+# remove G from qhost output in column $5 so I can treat it as a number
+# if mem total falls below preset output and store as MEM_CHECK variable.
+# if null then do nothing (in bash semicolon : means do nothing)
+# if variable is not null, send an email
 
-while true; 
+while sleep $REFRESH ;
 do
-	qhost \
+	MEM_CHECK=$(qhost \
 	| egrep -v "^-|global|node" \
 	| sed -r 's/[[:space:]]+/\t/g' \
 	| awk '{gsub(/G/,"",$5); print $0}' \
-	| awk '$1~"c6100-"&&$5<94 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1~"c6220-"&&$5<126 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1~"c6320-"&&$5<126 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1~"binabox"&&$5<126 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1~"DellR730-"&&$5<126 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1=="sunrhel3"&&$5<378 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1=="sunrhel4"&&$5<503 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"} \
-	$1=="Bright-VM"&&$5<7 {print $1 "\n" "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN"}' \
-	| mail -s \
-	DROPPED_MEMORY_DETECTED \
-	jhg-informatics@lists.johnshopkins.edu
+	| awk '$1~"c6100-"&&$5<94 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1~"c6220-"&&$5<126 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1~"c6320-"&&$5<126 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1~"binabox"&&$5<126 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1~"DellR730-"&&$5<126 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1=="sunrhel3"&&$5<378 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1=="sunrhel4"&&$5<503 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"} \
+	$1=="Bright-VM"&&$5<7 {print $1 , "HAS" , $5 , "Gb RAM, WHICH IS LESS THAN IT SHOULD HAVE. PLEASE DRAIN, REBOOT AND UNDRAIN" ";"}') ;
 
-	sleep $REFRESH
-
+	if [[ -z "$MEM_CHECK" ]]; then
+		:
+	else
+		echo $MEM_CHECK | sed 's/;/\n\n/g' | mail -s DROPPED_MEMORY_DETECTED jhg-informatics@lists.johnshopkins.edu
+	fi
 done
