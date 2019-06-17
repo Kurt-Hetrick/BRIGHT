f29c1c0baca89cb3844475444dea0706620ee84b
Kurt Hetrick <khetric1@jhmi.edu>
2019-06-17 12:02:51 -0400

    file_extension_size.sh: calculates how much space is being taken up by file extension in a given directory

---
+++ file_extension_size.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+INPUT_DIRECTORY=$1 # path to directory that you want to check file sizes on.
+
+ROW_COUNT=$2 # LIST THE TOP X NUMBER OF FILE EXTENSIONS ORDERED BY SIZE. DEFAULT IS 10
+
+		if [[ ! $ROW_COUNT ]]
+			then
+			ROW_COUNT=500
+		fi
+
+module load datamash
+
+find $INPUT_DIRECTORY -type f -exec du -a {} + \
+| awk 'BEGIN {FS="."} {print $1,$NF}' \
+| sed -r 's/[[:space:]]+/\t/g' \
+| sort -k 2,2 \
+| datamash -g 2 sum 1 \
+| sort -k 2,2nr 
+| awk 'BEGIN {print "EXTENSION" "\t" "SIZE_Gb"} {print $1 "\t" ($2/1024/1024)}' \
+| head $ROW_COUNT
