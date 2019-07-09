a32cf7d5b359f34adf3b4032c39d85496aac8fca
Kurt Hetrick <khetric1@jhmi.edu>
2019-07-09 14:59:33 -0400

    bug fixes for file_extension_size.sh

--- file_extension_size.sh
+++ file_extension_size.sh
@@ -2,7 +2,7 @@
 
 INPUT_DIRECTORY=$1 # path to directory that you want to check file sizes on.
 
-ROW_COUNT=$2 # LIST THE TOP X NUMBER OF FILE EXTENSIONS ORDERED BY SIZE. DEFAULT IS 10
+ROW_COUNT=$2 # LIST THE TOP X NUMBER OF FILE EXTENSIONS ORDERED BY SIZE. DEFAULT IS 15
 
 		if [[ ! $ROW_COUNT ]]
 			then
@@ -14,8 +14,8 @@ module load datamash
 find $INPUT_DIRECTORY -type f -exec du -a {} + \
 | awk 'BEGIN {FS="."} {print $1,$NF}' \
 | sed -r 's/[[:space:]]+/\t/g' \
-| sort -k 2,2 \
-| datamash -g 2 sum 1 \
-| sort -k 2,2nr 
+| sort -k 3,3 \
+| datamash -g 3 sum 1 \
+| sort -k 2,2nr \
 | awk 'BEGIN {print "EXTENSION" "\t" "SIZE_Gb"} {print $1 "\t" ($2/1024/1024)}' \
-| head $ROW_COUNT
+| head -n $ROW_COUNT
