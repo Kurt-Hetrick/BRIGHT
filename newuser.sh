b3f603dc020c140a3c4974037b74250fa2eb3fd3
Kurt Hetrick <khetric1@jhmi.edu>
2019-08-26 16:02:18 -0400

    added genetic analysts sub group to user set up so that they can have their own .bashrc

--- newuser.sh
+++ newuser.sh
@@ -28,6 +28,10 @@
 
 	ddl_users_regex="^(skight1|ktindal2|msherid3|ywang117|khetric1-clinical)$"
 
+# CREATE A REGEX FOR CIDR GENETIC ANALYSTS GROUP
+
+	cidr_genetic_analysts_regex="^(pzhang13|jzhang10|imcmull1|sserio1|kkindil1)$"
+
 # function for setting up standard (CIDR) users
 
 	SET_UP_NON_DDL_USERS ()
@@ -40,7 +44,7 @@
 					if [ -d "$priv" ]; then
 					    echo The $priv folder exists.
 					    echo
-					    cp -Rv /mnt/qadmin/users/skel/.defaultbashrc /mnt/qadmin/users/$username/priv/.bash_profile
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile /mnt/qadmin/users/$username/priv/.bash_profile
 					    echo
 						rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
 						echo
@@ -54,7 +58,7 @@
 					    echo
 					    echo No directory found $priv creating it
 					    echo
-					    cp -Rv /mnt/qadmin/users/skel/.defaultbashrc /mnt/qadmin/users/$username/priv/.bash_profile
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile /mnt/qadmin/users/$username/priv/.bash_profile
 					    echo
 						rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
 						echo
@@ -71,7 +75,7 @@
 				   echo
 				   mkdir -v $priv
 				   echo
-					cp -Rv /mnt/qadmin/users/skel/.defaultbashrc /mnt/qadmin/users/$username/priv/.bash_profile
+					cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile /mnt/qadmin/users/$username/priv/.bash_profile
 					echo
 					rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
 					echo
@@ -98,7 +102,7 @@
 					if [ -d "$priv" ]; then
 					    echo The $priv folder exists.
 					    echo
-					    cp -Rv /mnt/qadmin/users/skel/.defaultbashrc_ddl /mnt/qadmin/users/$username/priv/.bash_profile
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ddl /mnt/qadmin/users/$username/priv/.bash_profile
 					    echo
 						cp -Rv /mnt/qadmin/users/skel/cidrseqsuite_ddl.props /mnt/qadmin/users/$username/priv/cidrseqsuite.props
 					    echo
@@ -113,7 +117,7 @@
 					    mkdir -v $priv
 					    echo No directory found $priv creating it
 					    echo
-					    cp -Rv /mnt/qadmin/users/skel/.defaultbashrc_ddl /mnt/qadmin/users/$username/priv/.bash_profile
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ddl /mnt/qadmin/users/$username/priv/.bash_profile
 					    echo
 					    cp -Rv /mnt/qadmin/users/skel/cidrseqsuite_ddl.props /mnt/qadmin/users/$username/priv/cidrseqsuite.props
 					    echo
@@ -133,7 +137,7 @@
 				   echo
 				   mkdir -v $priv
 				   echo
-					cp -Rv /mnt/qadmin/users/skel/.defaultbashrc_ddl /mnt/qadmin/users/$username/priv/.bash_profile
+					cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ddl /mnt/qadmin/users/$username/priv/.bash_profile
 					echo
 					cp -Rv /mnt/qadmin/users/skel/cidrseqsuite_ddl.props /mnt/qadmin/users/$username/priv/cidrseqsuite.props
 					echo
@@ -151,11 +155,78 @@
 				fi
 		}
 
-# if the input username is DDL, then set up that person using ddl specific files, otherwise set them up as a standard user
+# function for setting up DDL users
 
-	if [[ $username =~ $ddl_users_regex ]] ; then
+	SET_UP_CIDR_GENETIC_ANALYSTS ()
+		{
+			#Check for the users directory
+				if [ -d "$dir" ]; then
+				   echo The $dir  folder exists.
+				   echo
+				   #Check for the priv directory
+					if [ -d "$priv" ]; then
+					    echo The $priv folder exists.
+					    echo
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ga /mnt/qadmin/users/$username/priv/.bash_profile
+					    echo
+						rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
+						echo
+					    ln -svf /mnt/users/skel/.sge_request /mnt/qadmin/users/$username/priv/.sge_request
+					    echo
+					    chmod -R 700 $priv
+					    echo
+				            chown -R win\\$username:win\\jhg_all $priv
+					else
+					    mkdir -v $priv
+					    echo
+					    echo No directory found $priv creating it
+					    echo
+					    cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ga /mnt/qadmin/users/$username/priv/.bash_profile
+					    echo
+						rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
+						echo
+					    ln -svf /mnt/users/skel/.sge_request /mnt/qadmin/users/$username/priv/.sge_request
+					    echo
+					    chmod -Rv 700 $priv
+					    echo
+				            chown -Rv win\\$username:win\\jhg_all $priv
+					fi
+				else
+				   echo No directory found $dir
+				   echo
+				   mkdir -v $dir
+				   echo
+				   mkdir -v $priv
+				   echo
+					cp -Rv /mnt/qadmin/users/skel/.defaultbash_profile_ga /mnt/qadmin/users/$username/priv/.bash_profile
+					echo
+					rm -rvf /mnt/qadmin/users/$username/priv/.sge_request
+					echo
+					ln -svf /mnt/users/skel/.sge_request /mnt/qadmin/users/$username/priv/.sge_request
+					echo
+					chmod -Rv 700 $priv
+					echo
+					chown -Rv win\\$username:win\\jhg_all $dir
+					echo
+				        chown -Rv win\\$username:win\\jhg_all $priv
+						echo
+				   echo "Users folders created"
+				fi
+		}
+
+
+# if the input username is DDL, then set up that person using ddl specific files
+# if the input username is CIDR genetic analyst, then set up that person using CIDR genetic analyst specific files
+#  otherwise set them up as a standard user
+
+	if [[ $username =~ $ddl_users_regex ]]
+		then
 			echo $username is a being set up as a DDL user
 			SET_UP_DDL_USERS
+		elif [[ $username =~ $cidr_genetic_analysts_regex ]]
+			then
+			echo $username is a being set up as a CIDR GENETIC ANALYST user
+			SET_UP_CIDR_GENETIC_ANALYSTS
 		else
 			echo $username is a being set up as a non-DDL, assuming CIDR, user
 			SET_UP_NON_DDL_USERS
