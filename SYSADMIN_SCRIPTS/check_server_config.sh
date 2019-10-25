3bf32d1a35ce7cffdd7bf8e241e0c6f46a60709f
Kurt Hetrick <khetric1@jhmi.edu>
2019-10-25 10:27:58 -0400

    changed how bright licensed server list is populated to account for longer host names

--- SYSADMIN_SCRIPTS/check_server_config.sh
+++ SYSADMIN_SCRIPTS/check_server_config.sh
@@ -7,6 +7,6 @@ user=$1
 		user=$(whoami)
 	fi
 
-for server in $(qconf -sep | egrep "cidr" | awk '{split($1,foo,"."); print foo[1]}'); \
+for server in $(qconf -sep | egrep -v "HOST|^=|SUM" | awk '{split($1,foo,"."); print foo[1]}'); \
 	do (ssh $user@$server uname -nr ; ssh $user@$server cat /etc/redhat-release; ssh $user@$server df -h /tmp | grep -v ^File) | paste - - - | grep -v Warning;
 done
