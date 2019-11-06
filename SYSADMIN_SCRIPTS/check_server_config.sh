fca63a9486a8b785b8a2602284509314eaf7a9a3
Kurt Hetrick <khetric1@jhmi.edu>
2019-11-06 11:00:49 -0500

    added uptime to check server config script. removed load averages from output

--- SYSADMIN_SCRIPTS/check_server_config.sh
+++ SYSADMIN_SCRIPTS/check_server_config.sh
@@ -8,5 +8,5 @@ user=$1
 	fi
 
 for server in $(qconf -sep | egrep -v "HOST|^=|SUM" | awk '{split($1,foo,"."); print foo[1]}'); \
-	do (ssh $user@$server uname -nr ; ssh $user@$server cat /etc/redhat-release; ssh $user@$server df -h /tmp | grep -v ^File) | paste - - - | grep -v Warning;
+	do (ssh $user@$server uname -nr ; ssh $user@$server uptime | cut -d , -f 1,2 ; ssh $user@$server cat /etc/redhat-release; ssh $user@$server df -h /tmp | grep -v ^File ) | paste - - - - | grep -v Warning;
 done
